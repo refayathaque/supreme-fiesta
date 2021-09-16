24464643b8a7c5c5f5aec51fe745d6e5ad6ccc2b
Refayat Haque <refayathaque@gmail.com>
2021-09-16 12:48:26 -0400

    getting the hang of context but SLOWLY

--- src/food-order-app/components/Meals/MealItemForm.js
+++ src/food-order-app/components/Meals/MealItemForm.js
@@ -1,10 +1,35 @@
 import Input from "../UI/Input.js";
 import classes from "./MealItemForm.module.css";
+import { useRef, useState } from "react";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    // important to note that ^ is always a string, even though we explicitly set the type to number below...
+    const enteredAmountNumber = +enteredAmount;
+    // ^ shorthand to convert string to a number
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+      // exiting out of this submitHandler method because our validation fails
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
+        // since ref isn't a property of props, it becomes a reserved keyword I guess
         label="Amount"
         input={{
           id: `amount${props.id}`,
@@ -16,9 +41,14 @@ const MealItemForm = (props) => {
           defaultValue: 1,
         }}
       />
-      <button type="button">+ Add</button>
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
 
 export default MealItemForm;
+
+// refs
+// good example of using refs and forward refs for 2 way data binding
+// Wrap the entire child element (Input in this case) with the forwardRef method - Input component becomes an argument to forwardRef method
--- src/food-order-app/store/cart-context.js
+++ src/food-order-app/store/cart-context.js
@@ -1,21 +1,57 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
   items: [],
   totalAmount: 0,
   addItem: (items) => {},
   removeItem: (id) => {},
+  // think of this as a blueprint for the context
 });
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  // state argument above corresponds to existing state, what you return will be a new state snapshot
+  // action argument above corresponds to what is coming in from the dispatch method
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    // important here to use concat instead of push because we don't want to mutate the existing array, we want to create a new one, we want to update state in an "immutable" way, we don't want to edit the old state snapshot because of the reference value thing in JS (meaning eixisting data in memory gets edited w/o React knowing about it), you want to generate a brand new state object which you return
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+    // returning the new state
+  }
+  return defaultCartState;
+};
 
 export const CartContextProvider = (props) => {
   // named export for this provider component
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  // useReducer: An alternative to useState. Accepts a reducer of type (state, action) => newState, and returns the current state paired with a dispatch method. (If you’re familiar with Redux, you already know how this works.)
+  // const [state, dispatch] = useReducer(reducer, initialArg, init);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
-    additem: addItemToCartHandler,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
 
@@ -27,3 +63,7 @@ export const CartContextProvider = (props) => {
 };
 
 export default CartContext;
+
+// refs:
+// Max went with useReducer instead of useState because in his mind this is a more complex piece of state to manage
+// Max also split this up to have a context file and a provider file, I'm just going to keep everything in one file
