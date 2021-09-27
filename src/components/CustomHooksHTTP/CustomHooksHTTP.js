e5bcc3b9f4636e3ae69d2d8224b31dc8a7d89619
Refayat Haque <refayathaque@gmail.com>
2021-09-26 22:02:49 -0400

    sleep

--- src/components/CustomHooksHTTP/CustomHooksHTTP.js
+++ src/components/CustomHooksHTTP/CustomHooksHTTP.js
@@ -13,9 +13,16 @@ const divStyle = {
 const CustomHooksHTTP = () => {
   const [tasks, setTasks] = useState([]);
 
-  // configuring and calling custom hook
   const subdirectory = "tasks.json";
   // https://blog.hubspot.com/marketing/parts-url
+
+  // calling custom hook and destructuring out what it returns
+  const {
+    sendRequest: fetchTasks, // using alias with ES6 obj destructuring to make it more relevant for what we're doing in this component
+    isLoading,
+    error,
+  } = useHttp();
+
   const transformTasks = useCallback((tasksObject) => {
     const loadedTasks = [];
     for (const taskKey in tasksObject) {
@@ -24,16 +31,11 @@ const CustomHooksHTTP = () => {
     setTasks(loadedTasks);
   }, []);
   // dependency array in this instance of `useCallback` can be left empty because `transformTasks` isn't using anything external, e.g., `setTasks` is a state updating function that's part of THIS component
-  const {
-    sendRequest: fetchTasks, // using alias with ES6 obj destructuring to make it more relevant for what we're doing in this component
-    isLoading,
-    error,
-  } = useHttp();
-
+  
   useEffect(() => {
     fetchTasks({ url: `${apis.baseUrl}${subdirectory}` }, transformTasks);
   }, [fetchTasks]);
-  // `useCallback` method wrapping the `sendRequest` method (`fetchTask` is an alias for that) in the custom hook, don't forget that `useCallback` also has dependencies, but in this case the `useCallback` method in the custom hook has no dependendencies as the `requestConfig` object and `applyData` method are being passed in as args, they're not really "external" dependencies since they're just args
+  // `useCallback` method wrapping the `sendRequest` method (`fetchTask` is an alias for that) in the custom hook, don't forget that `useCallback` also has a dependencies array as the 2nd arg, but in this case the `useCallback` method in the custom hook has no dependendencies as the `requestConfig` object and `applyData` method are being passed in as args, they're not really "external" dependencies since they're just args
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- src/components/CustomHooksHTTP/hooks/use-http.js
+++ src/components/CustomHooksHTTP/hooks/use-http.js
@@ -12,13 +12,13 @@ const useHttp = () => {
         method: requestConfig.method ? requestConfig.method : "GET",
         headers: requestConfig.headers ? requestConfig.headers : {},
         body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-        // making this super flexible with ternaries, if calling component needs to just make a GET request it won't need to provide extraneous information
+        // making this super flexible with ternaries, if the calling component needs to just make a GET request it won't need to provide extraneous information like method, headers and body
       });
       if (!response.ok) {
         throw new Error("Request failed!");
       }
       const data = await response.json();
-      // this custom hook is ONLY responsible of getting the data, how the data is transformed/used/etc. will be handled by the `applyData` method passed to this custom hook by the calling component
+      // this custom hook is ONLY responsible for getting the data, how the data is transformed/used/etc. will be handled by the `applyData` method passed into this `sendRequest` method by the calling component
       console.log(data);
       applyData(data);
     } catch (err) {
