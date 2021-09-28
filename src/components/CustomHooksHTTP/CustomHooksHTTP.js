1a55340499436a54c19cac49d9cc281ce90f60d9
Refayat Haque <refayathaque@gmail.com>
2021-09-27 21:59:09 -0400

    sleepy

--- src/components/CustomHooksHTTP/CustomHooksHTTP.js
+++ src/components/CustomHooksHTTP/CustomHooksHTTP.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 import apis from "../../apis";
@@ -23,19 +23,19 @@ const CustomHooksHTTP = () => {
     error,
   } = useHttp();
 
-  const transformTasks = useCallback((tasksObject) => {
-    const loadedTasks = [];
-    for (const taskKey in tasksObject) {
-      loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  }, []);
-  // dependency array in this instance of `useCallback` can be left empty because `transformTasks` isn't using anything external, e.g., `setTasks` is a state updating function that's part of THIS component
-  
   useEffect(() => {
+    const transformTasks = (tasksObject) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObject) {
+        loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
     fetchTasks({ url: `${apis.baseUrl}${subdirectory}` }, transformTasks);
   }, [fetchTasks]);
-  // `useCallback` method wrapping the `sendRequest` method (`fetchTask` is an alias for that) in the custom hook, don't forget that `useCallback` also has a dependencies array as the 2nd arg, but in this case the `useCallback` method in the custom hook has no dependendencies as the `requestConfig` object and `applyData` method are being passed in as args, they're not really "external" dependencies since they're just args
+  // `useCallback` method wrapping the `sendRequest` method (`fetchTask` is an alias for that) in the custom hook, don't forget that `useCallback` also has a dependencies array as the 2nd arg, but in this case the `useCallback` method in the custom hook has no dependendencies as the `requestConfig` object and `applyData` method are being passed in as args.
+  // They're not really "external" dependencies since they're just args. But had we passed in `requestConfig` and `applyData` through its parent method, the `useHttp` custom hook, then they WOULD be added to the `useCallback` dependency array because in that situation they ARE external dependencies.
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
