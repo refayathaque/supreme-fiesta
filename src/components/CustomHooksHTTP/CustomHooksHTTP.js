5c16f5a1f85a3ae3cf422d86dc0de33d18b5f81a
Refayat Haque <refayathaque@gmail.com>
2021-09-29 22:03:31 -0400

    sleep

--- src/components/CustomHooksHTTP/CustomHooksHTTP.js
+++ src/components/CustomHooksHTTP/CustomHooksHTTP.js
@@ -13,15 +13,15 @@ const divStyle = {
 const CustomHooksHTTP = () => {
   const [tasks, setTasks] = useState([]);
 
-  const subdirectory = "tasks.json";
-  // https://blog.hubspot.com/marketing/parts-url
-
   // calling custom hook and destructuring out what it returns
   const {
     sendRequest: fetchTasks, // using alias with ES6 obj destructuring to make it more relevant for what we're doing in this component
     isLoading,
     error,
   } = useHttp();
+
+  const subdirectory = "tasks.json";
+  // https://blog.hubspot.com/marketing/parts-url
 
   useEffect(() => {
     const transformTasks = (tasksObject) => {
@@ -34,12 +34,14 @@ const CustomHooksHTTP = () => {
 
     fetchTasks({ url: `${apis.baseUrl}${subdirectory}` }, transformTasks);
   }, [fetchTasks]);
+  // we need to wrap the method that is in the `useEffect` dependency array with `useCallback`, otherwise you'll get an infinite loop
   // `useCallback` method wrapping the `sendRequest` method (`fetchTask` is an alias for that) in the custom hook, don't forget that `useCallback` also has a dependencies array as the 2nd arg, but in this case the `useCallback` method in the custom hook has no dependendencies as the `requestConfig` object and `applyData` method are being passed in as args.
   // They're not really "external" dependencies since they're just args. But had we passed in `requestConfig` and `applyData` through its parent method, the `useHttp` custom hook, then they WOULD be added to the `useCallback` dependency array because in that situation they ARE external dependencies.
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
+  // important best-practice to use when updating lists in local state, `useState` `set...` method comes with `prevTasks` that we can use
 
   return (
     <div style={divStyle}>
--- src/components/CustomHooksHTTP/components/NewTask/NewTask.js
+++ src/components/CustomHooksHTTP/components/NewTask/NewTask.js
@@ -1,36 +1,44 @@
-import { useState } from "react";
 import apis from "../../../../apis";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // calling custom hook and destructuring out what it returns
+  const {
+    sendRequest: sendTaskRequest, // using alias with ES6 obj destructuring to make it more relevant for what we're doing in this component
+    isLoading,
+    error,
+  } = useHttp();
+
+  const subdirectory = "tasks.json";
+  // https://blog.hubspot.com/marketing/parts-url
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    const appendUrl = "tasks.json";
-    try {
-      const response = await fetch(`${apis.baseUrl}${appendUrl}`, {
+    sendTaskRequest(
+      {
+        url: `${apis.baseUrl}${subdirectory}`,
         method: "POST",
-        body: JSON.stringify({ text: taskText }),
         headers: {
           "Content-Type": "application/json",
         },
-      });
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-      const data = await response.json();
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+      // having to use `bind` here because in the customHook, `applyData`, which is the pointer to the `createTask` method here, only passes in a single arg - `applyData(data)` - but if you look up at `createText` it takes in two args, with `taskData` being the `data` in `applyData(data)` in the custom hook - we get around this problem by passing in `taskText` from here when passing in the `createTask` method as the second arg to `sendTaskRequest`
+      // What does the bind() method do though? It "prepares" the function on which it's called for future execution.
+      // It allows you to "preconfigure" which arguments that function should receive when it's eventually getting called.
+      // In addition, you can also define what the this keyword should refer to inside of that function, we "tell" JavaScript that this should be null (or, to be precise, not have any different value that it would have otherwise)
+      //You can simply keep in mind that the first parameter of bind is always the this keyword reference, all other parameters thereafter are the arguments fed into the function that will be invoked (i.e. on which you called bind).
+      // So the first parameter of the "to-be-called" function will then get the second argument passed to bind. The second parameter of the "to-be-called" function will be defined via the third argument passed to bind. And so on.
+      // https://academind.com/tutorials/function-bind-event-execution
+    );
   };
 
   return (
